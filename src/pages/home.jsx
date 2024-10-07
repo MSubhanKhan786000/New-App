e327e94a5a7ea15ea8a0e70d51010548d0ac2a20
Jawad Amir <jawad.amir237@gmail.com>
2024-10-08 03:57:52 +0500

    product detail complete

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { products } from "../products";
 import ProductCart from "../components/productCart";
 import { fetchProducts } from "../services/productService";
 import { useQuery } from "@tanstack/react-query";
@@ -9,16 +8,23 @@ const Home = () => {
     queryKey: ["products"],
     queryFn: fetchProducts,
   });
-  console.log(
-    "Fetched Products from Api Response:",
-    JSON.stringify(data, null, 2)
-  );
+
+  if (isLoading) {
+    return <div>Loading products...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching products!</div>;
+  }
+
+  const products = data?.data || [];
+
   return (
     <div>
       <h1 className="text-3xl my-5">List Products</h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
         {products.map((product, key) => (
-          <ProductCart key={key} data={product} />
+          <ProductCart key={product._id} data={product} />
         ))}
       </div>
     </div>
